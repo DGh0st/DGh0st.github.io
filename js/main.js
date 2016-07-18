da2dea620a085d5fcbac1ebd7cb5a9e4704a5395
DGh0st <deeppwnage@yahoo.com>
2016-07-18 18:20:53 -0500

    fixed navigation bar when window is small and fixed header image not displaying right when window is small

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 document.onscroll = function(){navbarmove()};
 document.onwheel = function(){navbarmove()};
+document.onresize = function(){changeNavFontSize()};
+document.onload = function(){changeNavFontSize()};
 
 function navbarmove() {
-    if(window.scrollY>=document.getElementById("mainbody").offsetTop){
+    if (window.scrollY >= document.getElementById("mainbody").offsetTop) {
             document.getElementById("navbar").style.top = "0px";
             document.getElementById("navbar").style.position = "fixed";
     } else {
