a774c1a44aca15f5100d2b71ba481aff7ffc3787
DGh0st <deeppwnage@yahoo.com>
2017-02-19 06:12:15 -0600

    Attempt at fixing the ios_version_check

--- Files/js/ios_version_check.js
+++ Files/js/ios_version_check.js
@@ -49,7 +49,7 @@ function ios_version_check(minIOS,maxIOS,otherIOS,callBack) {
 
 	var osVersion = [ version[2], version[3], version[4] ? version[5] : 0 ];
 	if (navigator.appVersion.match(/CPU(iPhone)? OS 10_(\d+)(_(\d+))? line/i)) {
-		alert(version);
+		osVersion = [ version[2], version[3], version[4], version[5] ? version[6] : 0];
 	}
 
 	var osString = osVersion[0] + "." + osVersion[1] + (osVersion[2] && osVersion[2] != 0 ? "." + osVersion[2] : ""),
