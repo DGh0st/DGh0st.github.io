6f10eb32fb0593a6bd49a71c1b03d5db3f2635eb
DGh0st <dgh0st@dgh0st.com>
2017-05-01 23:17:29 -0500

    Added a bash script to upload new tweaks/updates

---
+++ uploadUpdate.sh
@@ -0,0 +1,12 @@
+# Create Packages files
+./dpkg-scanpackages -m Files /dev/null > Packages
+rm Packages.gz Packages.bz2
+gzip -c9 Packages > Packages.gz
+bzip2 -c9 Packages > Packages.bz2
+
+# Upload to github with [message]
+echo "git commit message:"
+read message
+git add -A
+git commit -m "$message"
+git push
