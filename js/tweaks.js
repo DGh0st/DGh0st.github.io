456182394072ce1415fa7a905bb8d0e5ad0fe2d0
DGh0st <deeppwnage@yahoo.com>
2018-02-12 20:06:19 -0600

    Dynamically load the tweaks for projects page so they are easier to add more

--- js/tweaks.js
+++ js/tweaks.js
@@ -1,5 +1,381 @@
 window.onresize = function() { fixFirefoxResize(); }
 
+var iOSTweaks = { 
+	'showcase':[
+		{
+			name:'AlertClose', 
+			price:' (Free)', 
+			description:'Display an alert when you swip up an application in the application switcher.',
+			github:'https://www.github.com/DGh0st/AlertClose',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.alertclose/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.alertclose',
+			image:'img/AlertClose.png'
+		},
+		{
+			name:'BadgeOverlay',
+			price:' (Free)', 
+			description:'Give badges a more sleek look by making them overlays.',
+			github:'https://www.github.com/DGh0st/BadgeOverlay',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.badgeoverlay',
+			image:'img/BadgeOverlay.png'
+		},
+		{
+			name:'ScreenshotXI',
+			price:' ($1.00)',
+			description:'Brings iOS 11 style screenshotting to iOS 9 and 10.',
+			github:'https://www.github.com/DGh0st/ScreenshotXI',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.screenshotxi/',
+			image:'img/ScreenshotXI.png'
+		},
+		{
+			name:'SwitcherControls',
+			price:' ($1.50)',
+			description:'Have access to various control center sections while using deck switcher.',
+			github:'https://www.github.com/DGh0st/SwitcherControls',
+			cydiaRepo:'http://cydia.saurik.com/package/org.thebigboss.switchercontrols/',
+			image:'img/SwitcherControls.png'
+		}
+	],
+	'tweaks':[
+		{
+			name:'AlertClose', 
+			price:' (Free)', 
+			description:'Display an alert when you swip up an application in the application switcher.',
+			github:'https://www.github.com/DGh0st/AlertClose',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.alertclose/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.alertclose',
+			images:['img/AlertClose.png', 'img/AlertClosePreference.jpg']
+		},
+		{
+			name:'AppSwitchCurrentOrig', 
+			price:' (Free)', 
+			description:'Opens the app switcher to the current application instead of the last application.',
+			github:'https://www.github.com/DGh0st/AppSwitchCurrentOrig',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.appswitchcurrent10/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.appswitchcurrentOrig',
+			images:['img/AppSwitchCurrent.gif', 'img/AppSwitchCurrentPreference.jpg']
+		},
+		{
+			name:'BadgeOverlay',
+			price:' (Free)', 
+			description:'Give badges a more sleek look by making them overlays.',
+			github:'https://www.github.com/DGh0st/BadgeOverlay',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.badgeoverlay',
+			images:['img/BadgeOverlay.jpg']
+		},
+		{
+			name:'CenterObey',
+			price:' (Free)', 
+			description:'Open Control or Notification Center page corresponding to finger position.',
+			github:'https://www.github.com/DGh0st/CenterObey',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.centerobey',
+			images:['img/CenterObey.gif']
+		},
+		{
+			name:'CenterDockIcons', 
+			price:' (Free)', 
+			description:'Center the icons in the dock vertically.',
+			github:'https://www.github.com/DGh0st/CenterDockIcons',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.centerdockicons',
+			images:['img/CenterDockIcons.jpg']
+		},
+		{
+			name:'DarkAuxo', 
+			price:' (Free)', 
+			description:'Darken Control (iOS 8 - 10) and Notification Center (iOS 8 and 9 only).',
+			github:'https://www.github.com/DGh0st/DarkAuxo',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.darkauxo',
+			images:['img/DarkAuxo.png']
+		},
+		{
+			name:'DefaultPage', 
+			price:' (Free)', 
+			description:'Set the default homescreen and new applications download page.',
+			github:'https://www.github.com/DGh0st/DefaultPage',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.defaultpage',
+			images:['img/DefaultPage.gif']
+		},
+		{
+			name:'DelayVolChange', 
+			price:' (Free)', 
+			description:'Make first volume button press have no effect on volume level.',
+			github:'https://www.github.com/DGh0st/DelayVolChange',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.delayvolchange/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.delayvolchange',
+			images:['img/DelayVolChange.png']
+		},
+		{
+			name:'DVirtualHome', 
+			price:' (Free)', 
+			description:'Brings virtual home to iOS 10. Tap your home button instead of pressing it and add haptic feedback.',
+			github:'https://www.github.com/DGh0st/DVirtualHome',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.dvirtualhome',
+			images:['img/DVirtualHome.jpg']
+		},
+		{
+			name:'EnhancedSwitcherClose', 
+			price:' (Free)', 
+			description:'Display a view behind the swiped application in the switcher with unique actions at different vertical offset.',
+			github:'https://www.github.com/DGh0st/EnhancedSwitcherClose',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.enhancedswitcherclose/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.enhancedswitcherclose',
+			images:['img/EnhancedSwitcherClose.jpg', 'img/EnhancedSwitcherClose2.jpg', 'img/EnhancedSwitcherClose3.jpg']
+		},
+		{
+			name:'FloatingDock', 
+			price:' (Free)', 
+			description:'Gives the dock a floating look and makes the corners round.',
+			github:'https://www.github.com/DGh0st/FloatingDock',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.floatingdock',
+			images:['img/FloatingDock.png']
+		},
+		{
+			name:'Force3DAppShortcuts', 
+			price:' (Free)', 
+			description:'Enable 3D touch shortcuts on homescreen for older devices.',
+			github:'https://www.github.com/DGh0st/Force3DAppShortcuts',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.force3dappshortcuts/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.force3dappshortcuts',
+			images:['img/Force3DAppShortcuts.jpg']
+		},
+		{
+			name:'HideCCSwitchBackground', 
+			price:' (Free)', 
+			description:'Remove the background of buttons in the CC. Requires removal of white overlay using another tweak.',
+			github:'https://www.github.com/DGh0st/HideCCSwitchBackground',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.hideccswitchbackground',
+			images:['img/HideCCSwitchBackground.jpg']
+		},
+		{
+			name:'HomeCardIconLabel', 
+			price:' (Free)', 
+			description:'Add icon and label to the homescreen card. Change icon by replacing images at<br>/Library/Application Support/HomeCardIconLabel.',
+			github:'https://www.github.com/DGh0st/HomeCardIconLabel',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.homecardiconlabel/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.homecardiconlabel',
+			images:['img/HomeCardIconLabel.jpg']
+		},
+		{
+			name:'InvisibleHomeCard', 
+			price:' (Free)', 
+			description:'Make the homescreen card invisible inside app switcher to get the perception that it has been removed.',
+			github:'https://www.github.com/DGh0st/InvisibleHomeCard',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.invisblehomecard',
+			images:['img/InvisibleHomeCard.jpg']
+		},
+		{
+			name:'LSScreenshotLimit', 
+			price:' (Free)', 
+			description:'Limit the amount of screenshots that can be taken on the lockscreen with ability to fake screenshots.',
+			github:'https://www.github.com/DGh0st/LSScreenshotLimit',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.lsscreenshotlimit/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.lsscreenshotlimit',
+			images:['img/LSScreenshotLimit.jpg']
+		},
+		{
+			name:'NCAlwaysChevron', 
+			price:' (Free)', 
+			description:'Always display the chevron view in the notification center instead of switching to page dots once notfication center is fully displayed.',
+			github:'https://www.github.com/DGh0st/NCAlwaysChevron',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.ncalwayschevron/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.ncalwayschevron',
+			images:['img/NCAlwaysChevron.jpg']
+		},
+		{
+			name:'NCClearText', 
+			price:' (Free)', 
+			description:'Set custom text for Notification\'s "clear" and "No Notification" texts.',
+			github:'https://www.github.com/DGh0st/NCClearText',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.nccleartext',
+			images:['img/NCClearText.png']
+		},
+		{
+			name:'No3DTouchWidgets', 
+			price:' (Free)', 
+			description:'Remove the widgets from 3D touch shortcuts on homescreen.',
+			github:'https://www.github.com/DGh0st/No3DTouchWidgets',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.no3dtouchwidgets/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.no3dtouchwidgets',
+			images:['img/No3DTouchWidgets.jpg']
+		},
+		{
+			name:'NoHomeTapOnHS', 
+			price:' (Free)', 
+			description:'Disable the home button taps and presses on the homesceen.',
+			github:'https://www.github.com/DGh0st/NoHomeTapOnHS',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.nohometaponhs',
+			images:['img/NoHomeTapOnHS.png']
+		},
+		{
+			name:'NoLSNotificationBlur', 
+			price:' (Free)', 
+			description:'Remove the lockscren blur that is displayed underneath default lockscreen when there are notifications.',
+			github:'https://www.github.com/DGh0st/NoLSNotificationBlur',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.nolsnotificationblur',
+			images:['img/NoLSNotificationBlur.jpg']
+		},
+		{
+			name:'NoLSNotificationSeparators', 
+			price:' (Free)', 
+			description:'Remove the separators at the top and bottom of notifications list view when there are notifications on the lockscreen.',
+			github:'https://www.github.com/DGh0st/NoLSNotificationSeparators',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.nolsnotificationseparators',
+			images:['img/NoLSNotificationSeparators.jpg']
+		},
+		{
+			name:'NoUpsideDown', 
+			price:' (Free)', 
+			description:'Disable the upside down orientation throughout iOS.',
+			github:'https://www.github.com/DGh0st/NoUpsideDown',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.noupsidedown/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.noupsidedown',
+			images:['img/NoUpsideDown.png']
+		},
+		{
+			name:'ProductivityReminder', 
+			price:' (Free)', 
+			description:'Display an alert x minutes after application open to be reminded to get back to work.',
+			github:'https://www.github.com/DGh0st/ProductivityReminder',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.productivityreminder',
+			images:['img/ProductivityReminder.jpg']
+		},
+		{
+			name:'ReachOffset', 
+			price:' (Free)', 
+			description:'Customize many settings related to the reachability offset and adds option to have dynamic offset based on time between clicks.',
+			github:'https://www.github.com/DGh0st/ReachOffset',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.reachoffset/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.reachoffset',
+			images:['img/ReachOffset.png']
+		},
+		{
+			name:'SCAppShortcuts', 
+			price:' (Free)', 
+			description:'Add another section to SwitcherControls that contains a list of application which can be launched.',
+			github:'https://www.github.com/DGh0st/SCAppShortcuts',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.scappshortcuts',
+			images:['img/SCAppShortcuts.jpg']
+		},
+		{
+			name:'ScreenshotXI', 
+			price:' ($1.00)', 
+			description:'Brings iOS 11 style screenshotting to iOS 9 and 10.',
+			github:'https://www.github.com/DGh0st/ScreenshotXI',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.screenshotxi/',
+			images:['img/ScreenshotXI.jpg']
+		},
+		{
+			name:'SCTextSection', 
+			price:' (Free)', 
+			description:'Add section to SwitcherControls that contains custom text.',
+			github:'https://www.github.com/DGh0st/SCTextSection',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.sctextsection',
+			images:['img/SCTextSection.jpg']
+		},
+		{
+			name:'ShiftTopSwitcher', 
+			price:' (Free)', 
+			description:'Shifts the top application in the deck switcher so it seems the app switcher is open to current application.',
+			github:'https://www.github.com/DGh0st/ShiftTopSwitcher',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.shifttopswitcher',
+			images:['img/ShiftTopSwitcher.png']
+		},
+		{
+			name:'SwitcherControls', 
+			price:' ($1.50)', 
+			description:'Have access to various control center sections while using deck switcher.',
+			github:'https://www.github.com/DGh0st/SwitcherControls',
+			cydiaRepo:'http://cydia.saurik.com/package/org.thebigboss.switchercontrols/',
+			images:['img/SwitcherControls.jpg']
+		},
+		{
+			name:'UndoRotation', 
+			price:' (Free)', 
+			description:'Whenever you rotate your device, a button will be displayed on the screen allowing you to either undo rotation, lock orientation or both.',
+			github:'https://www.github.com/DGh0st/UndoRotation',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.undorotation/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.undorotation',
+			images:['img/UndoRotation.jpg']
+		},
+		{
+			name:'WhiteListSwitcherClose', 
+			price:' (Free)', 
+			description:'Create a whitelist of applications that restart, launch, or do nothing when you try to close it via application switcher.',
+			github:'https://www.github.com/DGh0st/WhiteListSwitcherClose',
+			cydiaRepo:'http://cydia.saurik.com/package/com.dgh0st.whitelistswitcher/',
+			betaRepo:'https://dgh0st.github.io/Files/?p=com.dgh0st.whitelistswitcher',
+			images:['img/WhitelistSwitcher.gif']
+		}
+	]
+};
+
+function addShowcases() {
+	var showcaseTweaksContainer = document.getElementsByClassName('runtime_modifications')[0];
+	var showcaseTweakTemplate = document.getElementById('showcaseTemplate');
+	Array.prototype.forEach.call(iOSTweaks['showcase'], function(tweakDict) {
+		var showcaseTweak = showcaseTweakTemplate.cloneNode(true);
+		showcaseTweak.id = "showcase" + tweakDict['name'];
+		var infoDiv = showcaseTweak.getElementsByClassName('info')[0];
+		var descriptionDiv = infoDiv.firstElementChild;
+		descriptionDiv.getElementsByClassName('name')[0].innerHTML = tweakDict['name'];
+		descriptionDiv.getElementsByClassName('price')[0].innerHTML = tweakDict['price'];
+		descriptionDiv.getElementsByTagName('p')[0].innerHTML = tweakDict['description'];
+		var linksDiv = infoDiv.getElementsByClassName('links')[0];
+		var linksChildNodes = linksDiv.children;
+		if (tweakDict['github'] == null)
+			linksChildNodes[0].style.display = 'none';
+		else
+			linksChildNodes[0].href = tweakDict['github'];
+		if (tweakDict['cydiaRepo'] == null)
+			linksChildNodes[1].style.display = 'none';
+		else
+			linksChildNodes[1].href = tweakDict['cydiaRepo'];
+		if (tweakDict['betaRepo'] == null)
+			linksChildNodes[2].style.display = 'none';
+		else
+			linksChildNodes[2].href = tweakDict['betaRepo'];
+		showcaseTweak.firstElementChild.lastElementChild.src = tweakDict['image'];
+		showcaseTweaksContainer.insertBefore(showcaseTweak, showcaseTweakTemplate);
+	});
+}
+
+function addAllTweaks() {
+	var allTweaksContainer = document.getElementsByClassName('allTweaks')[0];
+	var allTweakTemplate = document.getElementById('allTweakTemplate');
+	Array.prototype.forEach.call(iOSTweaks['tweaks'], function(tweakDict) {
+		var tweak = allTweakTemplate.cloneNode(true);
+		tweak.id = "tweak" + tweakDict['name'];
+		var imagesDiv = tweak.getElementsByClassName('images')[0];
+		var imageTemplate = document.getElementById('allTweakImageTemplate');
+		Array.prototype.forEach.call(tweakDict['images'], function(imageSrc, index) {
+			var image = imageTemplate.cloneNode(true);
+			image.id = tweakDict['name'] + "image" + index;
+			image.src = imageSrc;
+			imagesDiv.appendChild(image);
+		})
+		var infoDiv = tweak.getElementsByClassName('info')[0];
+		var descriptionDiv = infoDiv.firstElementChild;
+		descriptionDiv.getElementsByClassName('name')[0].innerHTML = tweakDict['name'];
+		descriptionDiv.getElementsByClassName('price')[0].innerHTML = tweakDict['price'];
+		descriptionDiv.getElementsByTagName('p')[0].innerHTML = tweakDict['description'];
+		var linksDiv = infoDiv.getElementsByClassName('links')[0];
+		var linksChildNodes = linksDiv.children;
+		if (tweakDict['github'] == null)
+			linksChildNodes[0].style.display = 'none';
+		else
+			linksChildNodes[0].href = tweakDict['github'];
+		if (tweakDict['cydiaRepo'] == null)
+			linksChildNodes[1].style.display = 'none';
+		else
+			linksChildNodes[1].href = tweakDict['cydiaRepo'];
+		if (tweakDict['betaRepo'] == null)
+			linksChildNodes[2].style.display = 'none';
+		else
+			linksChildNodes[2].href = tweakDict['betaRepo'];
+		allTweaksContainer.insertBefore(tweak, allTweakTemplate);
+	});
+}
+
 function displayAllTweaks() {
 	var showcaseTweaksElement = document.getElementsByClassName('runtime_modifications')[0];
 	document.getElementsByClassName('modifications')[0].style.transform = "translateX(-120%)";
@@ -8,14 +384,16 @@ function displayAllTweaks() {
 	setTimeout(function() {
 		var offset = 0;
 		Array.prototype.forEach.call(suppressedElements, function(element) {
-			setTimeout(function() {
-				element.style.display = "inline-block";
+			if (element.id !== "allTweakTemplate") {
 				setTimeout(function() {
-					element.style.opacity = 1;
-					element.style.transform = "translateY(0%) scaleX(1.0)";
-				}, 50);
-			}, offset * 100);
-			offset += 1;
+					element.style.display = "inline-block";
+					setTimeout(function() {
+						element.style.opacity = 1;
+						element.style.transform = "translateY(0%) scaleX(1.0)";
+					}, 50);
+				}, offset * 100);
+				offset += 1;
+			}
 		});
 	}, 250);
 }
